5d8254f4622a6424e7f5dffe3b89a82826924ebc
jsjshub <iamjsjs0319@gmail.com>
2021-10-06 10:39:50 +0900

    :+1:

---
+++ jsjshub/front/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php
+    include_once "../front/base/b_header.php"
+    ?>
+</head>
+<body>
+<div class="alert alert-primary" role="alert">
+    A simple primary alert—check it out!
+</div>
+</body>
+</html>
