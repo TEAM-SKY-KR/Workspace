959e86cf6d54afc54a8eee3f1e044035b28a67ad
wono9962 <wono9962@gmail.com>
2021-10-06 12:08:28 +0900

    add index_front_menu

---
+++ wono9962/front/menu.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap" rel="stylesheet">
+    <?php
+    include_once "../front/base/b_header.php"
+    ?>
+    <style>
+        .navbar{
+            background-color: #4174B9;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="..\front\img\logo.png" alt="" width="35" height="29">
+            <span class="fw-bolder text-light">위드 대구</span>
+        </a>
+        <button class="navbar-toggler fw-bolder text-white rounded" type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link py-3 rounded text-light" href="index.php"><i
+                                class="fas fa-home"></i></a></li>
+                <li class="nav-item"><a class="nav-link py-3 text-light" href="tourlist.html">대표 관광명소</a>
+                </li>
+                <li class="nav-item"><a class="nav-link py-3 text-light" href="#">스탬프</a></li>
+                <li class="nav-item"><a class="nav-link py-3 text-light" href="#">먹거리</a></li>
+                <li class="nav-item"><a class="nav-link py-3 text-light" href="login.html"><i
+                                class="fas fa-user-check"></i></a></li>
+                <li class="nav-item">
+                    <form class="d-flex input-group mx-0">
+                        <input class="form-control" type="search" placeholder="Search">
+                        <span class="input-group-text"><a class="nav-link rounded" href="#">
+                            <i class="fas fa-search text-dark"></i>
+                        </a></span>
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+            <div>sr</div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
