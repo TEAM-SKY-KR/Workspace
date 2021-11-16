16b8d8172d77d911c522326a895cd43803718169
wono9962 <wono9962@gmail.com>
2021-11-17 02:52:50 +0900

    add tourlist page

---
+++ wono9962/aa_update/front/member/join/js/user_validation.js
@@ -0,0 +1,106 @@
+function data_validation() {
+    // 빈칸 확인, 8자리 이상인지 확인  확인 하고 서버로 전송
+    const id = document.getElementById("id");
+    const email = document.getElementById("email");
+    const pw = document.getElementById("pw");
+    const pw2 = document.getElementById("pw2");
+    const check = document.getElementById("check");
+
+    // 빈칸 확인
+    if (id.value == "") {
+        Swal.fire(
+            '아이디오류!',
+            '아이디를 입력해주세요!',
+            'question'
+        )
+        id.focus();
+        return;
+    } else if (email.value == "") {
+        Swal.fire(
+            '이메일오류!',
+            '이메일을 입력해주세요!',
+            'question'
+        )
+        email.focus();
+        return;
+    } else if (pw.value == "" || pw2.value == "") {
+        Swal.fire(
+            '비밀번호오류!',
+            '비밀번호를 입력해주세요!',
+            'question'
+        )
+        pw.focus();
+        return;
+    } else if (pw.value != pw2.value) {
+        Swal.fire(
+            '비밀번호오류!',
+            '비밀번호가 맞지 않습니다.',
+            'question'
+        )
+        pw.focus();
+        return;
+    } else {
+        post_data();
+    }
+}
+
+
+function post_data() {
+    let queryString = $("form[name=user_register_form]").serialize();
+    $.ajax({
+        type: 'post',
+        url: 'user_join_data.php',
+        data: queryString,
+        dataType: 'text',
+        success: function (data) {
+
+           if(data=="ok"){
+               Swal.fire({
+                   position: 'center',
+                   icon: 'success',
+                   title: '회원가입이 완료되었습니다.',
+                   showConfirmButton: false,
+                   timer: 500
+               })
+               setTimeout(function () {
+                   location.replace("../../index.php");
+               }, 540);
+           }else{
+               Swal.fire(
+                   '오류!',
+                   '이미 존재하는 아이디 입니다.',
+                   'error'
+               )
+           }
+        }
+    });
+}
+
+// ----------------------------------
+
+function id_double_check() {
+    let queryString = $("input[name=id]").serialize();
+    $.ajax({
+        type: 'post',
+        url: 'user_join_id.php',
+        data: queryString,
+        dataType: 'text',
+        success: function (data) {
+            if (data == "ok") {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: '사용가능한 아이디',
+                    showConfirmButton: false,
+                    timer: 500
+                })
+            } else if (data == "no") {
+                Swal.fire(
+                    '아이디오류!',
+                    '이미 존재하는 아이디 입니다.',
+                    'error'
+                )
+            }
+        }
+    });
+}
---
+++ wono9962/aa_update/front/tourlist/tourlist.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php
+    include_once "../base/b_head.php"
+    ?>
+    <link href="../css/styles.css" rel="stylesheet"/>
+    <script src="../js/scripts.js"></script>
+</head>
+<body>
+<?php
+include_once "../header/menu.php";
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-12 mt-2">
+            <div class="row">
+                <div class="col-12">
+                    <hr>
+                    <h3 class="py-2"><i class="fas fa-map-marked-alt px-3"></i>대구 관광지 목록</h3>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/83tower.jpg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">83타워</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/228.jpg" alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">2.28공원</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/catholic.jpg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">성당</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/catholic.jpg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">성당</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/Susung.jpeg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">수성못</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/228.jpg" alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">83타워</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/83tower.jpg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3-3  w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">83타워</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                            <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+                <div class="col-lg-3 mb-5">
+                    <div class="mb-3">
+                        <!-- Product image-->
+                        <img class="card-img-top rounded-top-3 w-100 h-100" src="./image/catholic.jpg"
+                             alt="..."/>
+                        <!-- Product actions-->
+                        <div class="card-footer bg-dark rounded-bottom-3 w-100">
+                            <div class="text-center">
+                                <a class="text-decoration-none text-white" href="#">
+                                    <h5 class="mt-1">성당</h5>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-phone"></i></span>
+                        <span class="ms-1">053-111-2222</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fas fa-map-marked-alt"></i></span>
+                        <span class="ms-1">대구 달서구 두류공원로 200</span>
+                    </div>
+                    <div class="mt-2">
+                        <span class="pe-2"><i class="fab fa-internet-explorer"></i></span>
+                        <span class="ms-1">
+                                    <a href="#" class="text-decoration-none text-black">http://www.eworld.kr</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
