1ef16af343ce160ff883ccc6a5f8f6779d683239
Hi <68013103+jsjshub@users.noreply.github.com>
2021-11-21 23:25:01 +0900

    :open_file_folder:

---
+++ jsjshub/CSharp/Capstone_Landmark/Capstone_Landmark/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capstone_Landmark
+{
+    public partial class Form1 : Form
+    {
+        //이미지 분류 전역변수
+        public static ArrayList image_url = new ArrayList();
+        //관광지 타이틀 전역변수
+        public static ArrayList landmark_title = new ArrayList();
+        //관광지 설명 전역변수
+        public static ArrayList landmark_explain = new ArrayList();
+        //관광지 전화번호 전역변수
+        public static ArrayList landmark_phone = new ArrayList();
+        //관광지 주소 전역변수
+        public static ArrayList landmark_address = new ArrayList();
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            for (int i = 1; i <= 25; i++)
+            {
+                //httprequest 받아오기
+                string url = "https://tour.daegu.go.kr/index.do?menu_id=00002943";
+                string temp = HttpRequest.Result_Post(url, i);
+
+
+                Console.WriteLine(count);
+                //이미지분류
+                Regex reg = new Regex(@"<imgsrc=""/icms/tour(.+)""alt=");
+                MatchCollection resultColl = reg.Matches(temp.Replace(" ", String.Empty));
+
+
+
+                foreach (Match mm in resultColl)
+                {
+                    image_url.Add("https://tour.daegu.go.kr/icms/tour" + mm.Groups[1].ToString().Trim());
+                    Console.WriteLine(mm.Groups[1].ToString().Trim());
+                }
+
+                //관광지분류
+                reg = new Regex(@"class=""sub_box_title_nav"">(.+)</a>");
+                resultColl = reg.Matches(temp);
+
+
+                foreach (Match mm in resultColl)
+                {
+                    landmark_title.Add(mm.Groups[1].ToString().Trim());
+                    Console.WriteLine(mm.Groups[1].ToString().Trim());
+                }
+
+
+
+                //관광지 설명
+                reg = new Regex(@"<p class=""black_line_ct"">(.+)</p>");
+                resultColl = reg.Matches(temp);
+
+
+                foreach (Match mm in resultColl)
+                {
+                    landmark_explain.Add(mm.Groups[1].ToString().Trim());
+                    Console.WriteLine(mm.Groups[1].ToString().Trim());
+                }
+
+
+
+                //관광지 전화번호
+                reg = new Regex(@"<span class=""adress1"">(.+)</span>");
+                resultColl = reg.Matches(temp);
+
+
+                foreach (Match mm in resultColl)
+                {
+                    landmark_phone.Add(mm.Groups[1].ToString().Trim());
+                    Console.WriteLine(mm.Groups[1].ToString().Trim());
+                }
+
+                //관광지 주소
+                reg = new Regex(@"<span class=""adress"">(.+)</span>");
+                resultColl = reg.Matches(temp.Replace("\r",string.Empty));
+
+
+                foreach (Match mm in resultColl)
+                {
+                    landmark_address.Add(mm.Groups[1].ToString().Trim());
+                    Console.WriteLine( mm.Groups[1].ToString().Trim());
+                }
+                count++;
+            }
+       
+            //for (int j = 0; j<image_url.Count; j++)
+            //{
+            //    string textValue = j + "번|"  + "|" + landmark_address[j] + "|" + landmark_phone[j] + "|" + image_url[j] + "|\n";
+            //    textBox1.AppendText(textValue);
+
+            //}
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string url = "https://tour.daegu.go.kr/index.do?menu_id=00002943";
+            string temp = HttpRequest.Result_Post(url, 1);
+            Console.WriteLine(temp);
+        }
+    }
+}
---
+++ jsjshub/CSharp/Capstone_Landmark/Capstone_Landmark/HttpRequest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone_Landmark
+{
+    internal class HttpRequest
+    {
+        public static string Result_Get(string url_text)
+        {
+            string temp = null;
+            string url = url_text;  //테스트 사이트
+            string responseText = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Timeout = 30 * 1000; // 30초
+            request.Headers.Add("Authorization", "BASIC SGVsbG8="); // 헤더 추가 방법
+
+            using (HttpWebResponse resp = (HttpWebResponse)request.GetResponse())
+            {
+                HttpStatusCode status = resp.StatusCode;
+
+
+                Stream respStream = resp.GetResponseStream();
+                using (StreamReader sr = new StreamReader(respStream, Encoding.Default, true))
+                {
+                    responseText = sr.ReadToEnd();
+                }
+            }
+
+
+            temp = responseText;
+            return temp;
+        }
+
+        public static string Result_Post(string url_text, int i)
+        {
+            string temp = null;
+
+            string url = url_text;
+            string data = "tourId=&pageIndex="+i+"&searchCondition2=1&searchCondition3=&searchCondition4=&view_keyword=&view_category2=&keyword=&searchKeyword=";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Timeout = 30 * 1000;
+            //request.Headers.Add("Authorization", "BASIC SGVsbG8=");
+
+            // POST할 데이타를 Request Stream에 쓴다
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+            request.ContentLength = bytes.Length; // 바이트수 지정
+
+            using (Stream reqStream = request.GetRequestStream())
+            {
+                reqStream.Write(bytes, 0, bytes.Length);
+            }
+
+            // Response 처리
+            string responseText = string.Empty;
+            using (WebResponse resp = request.GetResponse())
+            {
+                Stream respStream = resp.GetResponseStream();
+                using (StreamReader sr = new StreamReader(respStream))
+                {
+                    responseText = sr.ReadToEnd();
+                }
+            }
+
+            temp = responseText;
+            return temp;
+        }
+    }
+}
