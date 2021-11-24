9c826da37a17e1d39c40ce207a46ac9429db0084
jsjshub <iamjsjs0319@gmail.com>
2021-11-24 09:51:37 +0900

    :file_folder:

--- jsjshub/CSharp/Capstone_Landmark/Capstone_Landmark/Form1.Designer.cs
+++ jsjshub/CSharp/Capstone_Landmark/Capstone_Landmark/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -43,31 +43,33 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 40);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(108, 100);
-            this.textBox1.TabIndex = 3;
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(205, 208);
+            this.button2.Location = new System.Drawing.Point(12, 40);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(108, 22);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "정규화";
+            this.button2.Size = new System.Drawing.Size(108, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "텍스트 로드";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.Click += new System.EventHandler(this.button2_Click_2);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "이미지 다운로드";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(518, 439);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
@@ -76,15 +78,14 @@
             this.ShowIcon = false;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
