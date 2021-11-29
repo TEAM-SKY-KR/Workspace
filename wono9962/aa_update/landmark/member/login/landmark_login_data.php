7d205a576237440a1e9ec177086af88aa122b529
wono9962 <wono9962@gmail.com>
2021-11-29 15:20:17 +0900

    add login & join DB connect in landmark

---
+++ wono9962/aa_update/landmark/member/login/landmark_login_data.php
@@ -0,0 +1,27 @@
+<?php
+//ini_set('display_errors', '0');
+include_once "../../base/setup2.php";
+
+$id = $_POST["id"];
+$pw = $_POST["pw"];
+
+
+$sql = "SELECT * FROM member_landmark WHERE username = '$id'";
+$result = mysqli_query($conn, $sql);
+
+
+$row = mysqli_fetch_array($result);
+
+if ($row["username"] == $id) {
+    if (password_verify($pw, $row["pw"])) {
+        echo "ok";
+        session_start();
+        $_SESSION['username'] = $id;
+    } else {
+        echo "no";
+    }
+} else {
+    echo "no";
+}
+?>
+
